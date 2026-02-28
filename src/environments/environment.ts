e3dae16cc4701aabe3433601bcbc1c0662a16314
Paula Haidamac <haidamacpaula@gmail.com>
2026-02-28 23:56:19 +0200

    ISX-35: Configured dev&prod domains with firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCp9G9mBWk46_IB2rOYsmsyPRUDW0jy-gQ",
+        authDomain: "isdox-dev.firebaseapp.com",
+        projectId: "isdox-dev",
+        storageBucket: "isdox-dev.firebasestorage.app",
+        messagingSenderId: "336461435923",
+        appId: "1:336461435923:web:3be194aa4093d7e7ffdbe6",
+        measurementId: "G-FNBV7BEG0E"
+    }
+};
